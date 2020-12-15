55f883d27dbcd40826a91582eab4110fe21b51e7
Margaret Lundgren <margareteliselundgren@gmail.com>
2020-12-15 19:46:37 +0000

    Added const catArray with game tiles to js file

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+$(function(){
+
+//cat tiles
+const catArray = [
+    {
+        name: 'bandana',
+        img: 'images/bandana.png'
+    },
+    {
+        name: 'bathe',
+        img: 'images/bathe.png'
+    },
+    {
+        name: 'bow',
+        img: 'images/bow.png'
+    },
+    {
+        name: 'box',
+        img: 'images/box.png'
+    },
+    {
+        name: 'fluffy',
+        img: 'images/fluffy.png'
+    },    
+    {
+        name: 'food',
+        img: 'images/food.png'
+    },
+        
+    {
+        name: 'hat',
+        img: 'images/hat.png'
+    },    
+    {
+        name: 'layin',
+        img: 'images/layin.png'
+    },    
+    {
+        name: 'leaves',
+        img: 'images/leaves.png'
+    },    
+    {
+        name: 'magazine',
+        img: 'images/magazine.png'
+    },    
+    {
+        name: 'party',
+        img: 'images/party.png'
+    },    
+    {
+        name: 'rug',
+        img: 'images/rug.png'
+    },    
+    {
+        name: 'sleep',
+        img: 'images/sleep.png'
+    },    
+    {
+        name: 'walking',
+        img: 'images/walking.png'
+    },    
+    {
+        name: 'yarn',
+        img: 'images/yarn.png'
+    }
+];
+});
